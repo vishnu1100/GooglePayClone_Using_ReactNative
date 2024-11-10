fe6d800a0e870981cbddc38a1d5b603122199e39
Vishnu <vishnusanthoshvr@gmail.com>
2024-11-10 18:07:12 +0530

    Added new page in profile

---
+++ components/profile_Page.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function profile_Page() {
+  return (
+    <View>
+      <Text style={styles.mainText}>profile_Page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+    mainText:{
+        flex:1,
+    }
+
+})
